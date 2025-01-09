684f18ddc334a57782b59118815aede0c0213d4c
ELSOLRA <bardakasmiljo@gmail.com>
2025-01-09 10:06:34 +0100

    update function on ChiprController, chirp policy, and some restyling

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
             'chirps' => Chirp::with('user')->latest()->get(),
@@ -58,9 +61,17 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate(
+            ['message' => 'required|string|max:255',]
+
+        );
+
+        $chirp->update($validated);
+        return redirect(route('chirps.index'));
     }
 
     /**
--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -13,9 +13,9 @@ const form = useForm({
 </script>
 
 <template>
-    <AuthenticatedLayout>
-        <Head title="Chirps" />
+    <Head title="Chirps" />
 
+    <AuthenticatedLayout>
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
             <form
                 @submit.prevent="
@@ -28,12 +28,12 @@ const form = useForm({
                     v-model="form.message"
                     placeholder="What's on your mind?"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                />
+                ></textarea>
                 <InputError :message="form.errors.message" class="mt-2" />
                 <PrimaryButton class="mt-4">Chirp</PrimaryButton>
             </form>
 
-            <div class="mt-6 rounded-lg divide-y">
+            <div class="mt-6 shadow-sm rounded-lg divide-y">
                 <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
             </div>
         </div>
