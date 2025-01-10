3c9d9d0f6632e1381ab73ded129bc65a404d58af
ELSOLRA <bardakasmiljo@gmail.com>
2025-01-10 09:41:31 +0100

    added event ChirpCreated class,  listner, updated Chirp model, created notification with NewChirp class, added factory to Chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['message'];
 
-    public function user()
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
